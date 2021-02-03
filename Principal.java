cd090dade75276eda3b2bdc8f8ccffef95ee578e
Gabriel <gabriel@pop-os.localdomain>
2021-02-03 09:52:51 -0300

    Inicio no desenvolvimento do trabalho

---
+++ Principal.java
@@ -0,0 +1,19 @@
+package Raposas_e_Coelhos_simulacao;
+
+/**
+ *  Trabalho final de Práticas de Programação Orientada a Objetos
+ * 
+ * @author Gabriel Henrique Silva Bernardo
+ * @author Lucas Floriano Bernardes de Castro
+ * @author Kleber Halley Sallum Guimarães
+ * @author Paulo Eduardo Tomaz de Biaso 
+ */
+
+ 
+public class Principal{
+  public static void main(String[] args){
+    Simulator simulator = new Simulator();
+    //simulator.runLongSimulation();
+    //simulator.simulate(300);
+  }
+}
